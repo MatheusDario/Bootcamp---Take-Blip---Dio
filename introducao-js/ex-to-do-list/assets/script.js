910c8904edb41a82f6e65feca3d83e7614b42e87
MatheusDario <dario.matheus97@gmail.com>
2022-02-08 13:38:33 -0300

    desafio to-do-list concluido

---
+++ introducao-js/ex-to-do-list/assets/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("task-form");
+const listaTarefas = document.getElementById("tasks");
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const campoDigitado = document.getElementById("task-input");
+    addTarefa(campoDigitado.value);
+    form.reset();
+}
+
+function addTarefa(digitada) {
+    const tarefaContainer = document.createElement('div');
+	const novaTarefa = document.createElement('input');
+	const tarefaLabel = document.createElement('label');
+	const tarefaDigitadaNode = document.createTextNode(digitada);
+
+	novaTarefa.setAttribute('type', 'checkbox');
+	novaTarefa.setAttribute('name', digitada);
+	novaTarefa.setAttribute('id', digitada);
+
+	tarefaLabel.setAttribute('for', digitada);
+	tarefaLabel.appendChild(tarefaDigitadaNode);
+
+	tarefaContainer.classList.add('task-item');
+	tarefaContainer.appendChild(novaTarefa);
+	tarefaContainer.appendChild(tarefaLabel);
+
+	listaTarefas.appendChild(tarefaContainer);
+}
