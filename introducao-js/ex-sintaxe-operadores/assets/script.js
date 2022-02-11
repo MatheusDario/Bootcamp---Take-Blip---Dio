585a6c8583aa00deeeeda5c07e9c7a0f31c4ad76
MatheusDario <dario.matheus97@gmail.com>
2022-02-11 18:19:40 -0300

    desafio sintaxe concluido

---
+++ introducao-js/ex-sintaxe-operadores/assets/script.js
@@ -0,0 +1,12 @@
+function verificarNumeros(a, b) {
+    var soma = a + b;
+    
+    
+    if(a === b) {
+        return `Os números ${a, b} são iguais. Sua soma e ${soma}, ${verficaMaiorDez}, ${verficaMaiorVinte}`
+    } else {
+        return `Os números ${a} e ${b} não são iguais. Sua soma e ${soma}, ${verficaMaiorDez}, ${verficaMaiorVinte}.`
+    }
+}
+
+console.log(verificarNumeros(20 , 20))
