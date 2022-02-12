115d52c968534f5e3c133adc38b595d3df882172
MatheusDario <dario.matheus97@gmail.com>
2022-02-12 14:26:37 -0300

    desafio sintaxe refeito

--- introducao-js/ex-sintaxe-operadores/assets/script.js
+++ introducao-js/ex-sintaxe-operadores/assets/script.js
@@ -1,12 +1,26 @@
 function verificarNumeros(a, b) {
-    var soma = a + b;
-    
+    const resultado = somaNumeros(a,b);
     
     if(a === b) {
-        return `Os números ${a, b} são iguais. Sua soma e ${soma}, ${verficaMaiorDez}, ${verficaMaiorVinte}`
+        return `Os números ${a, b} são iguais. Sua soma e ${resultado}`
     } else {
-        return `Os números ${a} e ${b} não são iguais. Sua soma e ${soma}, ${verficaMaiorDez}, ${verficaMaiorVinte}.`
+        return `Os números ${a} e ${b} não são iguais. Sua soma e ${resultado}`
     }
 }
 
-console.log(verificarNumeros(20 , 20))
+function somaNumeros(a, b) {
+    const soma = a + b;
+    let comparaDez = 'menor';
+    let comparaVinte = 'menor';
+
+    if (soma > 10) {
+        comparaDez = 'maior';
+    }
+    if (soma > 20) {
+        comparaVinte = 'maior';
+    }
+
+    return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`;
+}
+
+console.log(verificarNumeros(9 , 2))
